17820b3fa9b32f09d94b5e0cde9898b2b13a98a3
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-11-03 19:01:35 +0200

    first

--- Laba 5.1/Laba 5.1.cpp
+++ Laba 5.1/Laba 5.1.cpp
@@ -6,7 +6,7 @@ double f(const double a, const double b, const double c);
 int main()
 {
 	double s, t;
-		cout << "s = "; cin >> s;
+	cout << "s = "; cin >> s;
 	cout << "t = "; cin >> t;
 	double c = (f(t, s, 2) + f(1, s + t , t - s)) / (1 + f(1,1,t*t+s*s)* f(1, 1, t * t + s * s));
 	cout << "c = " << c << endl;
