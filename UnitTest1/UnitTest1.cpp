716bf2918e75c9597540e912a7ac9dcac9806dcc
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-11-03 19:43:57 +0200

    second

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = (0, 0);
+			Assert::AreEqual(t, 0);
+
+		}
+	};
+}
